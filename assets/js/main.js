2a66b564f7731f2bf219d077686614d11624a407
brent philip <brentagetrophil@gmail.com>
2021-07-30 20:23:38 +0800

    nn

---
+++ assets/js/main.js
@@ -0,0 +1,57 @@
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const navItems = document.querySelectorAll(".block");
+const navItem = document.querySelectorAll(".nav-item");
+let showMenu = false;
+menuBtn.addEventListener("click", toggleMenu);
+navItem.forEach((item) => item.addEventListener("click", toggleMenu));
+function toggleMenu() {
+    if (!showMenu) {
+        menuBtn.classList.add("close");
+        menu.classList.add("show");
+        menuNav.classList.add("show");
+        navItems.forEach((item) =>
+        item.classList.add("show"));
+        navItem.forEach((item) =>
+        item.classList.add("show"));
+        navItem.forEach((item) =>
+        item.classList.remove("close"));
+        showMenu = true;
+        navItems.forEach((item) =>
+        item.classList.remove("close"));
+        showMenu = true;
+    } else {
+        menuBtn.classList.remove("close");
+        menu.classList.remove("show");
+        menuNav.classList.remove("show");
+        navItems.forEach((item) =>
+        item.classList.add("close"));
+        showMenu = false;
+        navItem.forEach((item) =>
+        item.classList.add("close"));
+        showMenu = false;
+    }
+};
+
+// const next = document.querySelector(".buttonleft");
+// const prev = document.querySelector(".buttonright");
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items: 1,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    nav:false,
+    // dots:false,
+    // navText:[
+    //   next,prev
+    // ],
+    autoplayTimeout:5000,
+    autoplayHoverPause:true
+});
+
+$('.testnav').on('click', '.dot', function(e) {
+  owl.trigger('to.owl.carousel', [jQuery(this).index(), 300]);
+});
